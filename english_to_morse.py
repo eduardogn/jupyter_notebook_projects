f07646e04981c7f0ff1d16a44ceceb0f57ada47d
Eduardo Niederauer <eduardoniederauer@Eduardos-MacBook-Air.local>
2024-07-30 17:25:13 +0100

    Added a .py version of the englush to morse translator

---
+++ english_to_morse.py
@@ -0,0 +1,28 @@
+#Testing the commit
+
+english_to_morse ={
+    'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',   'E': '.',
+    'F': '..-.',  'G': '--.',   'H': '....',  'I': '..',    'J': '.---',
+    'K': '-.-',   'L': '.-..',  'M': '--',    'N': '-.',    'O': '---',
+    'P': '.--.',  'Q': '--.-',  'R': '.-.',   'S': '...',   'T': '-',
+    'U': '..-',   'V': '...-',  'W': '.--',   'X': '-..-',  'Y': '-.--',
+    'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'
+}
+stop = ""
+while stop != "Yes":
+  morse_code = []
+  text = input("Type the word to be translated to Morse code: ")
+
+  def translation (text):
+    for char in text.upper():
+      morse_code.append(english_to_morse[char])
+
+    return ' '.join(morse_code)
+
+
+  morse_code_output = translation(text)
+  print("Morse code:", morse_code_output)
+  stop = input("Do you want to stop translating? (Yes/No) ")
+
